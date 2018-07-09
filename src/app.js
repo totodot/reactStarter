5e7307c2053805f0a5b5b0531aa563d71d3edf67
totodot <toto.zetka@gmail.com>
2018-07-09 20:27:07 +0200

    Create React App Starter

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import totoImg from './images/toto.jpg';
+import './styles.scss';
+
+const logoStyles = {
+  width: '100px',
+};
+const App = () => (
+  <div>
+    <h2>Toto React App Starter</h2>
+    <img src={totoImg} alt="Toto Logo" style={logoStyles} />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              // modular styles
+              // modules: true,
+              // importLoaders: 1,
+              // localIdentName: "[name]_[local]_[hash:base64]",
+              // minimize: true
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
